289ee1bb88e677ef7c39059ea2bae7c085271796
Arun Prakasan AKV <afm447@gmail.com>
2017-06-10 14:49:56 +0530

    Create work.R

--- work.R
+++ work.R
@@ -9,3 +9,11 @@ x <- c(1:5);y<-x
 
 # dot chart
  dotchart(mtcars$mpg,labels = row.names(mtcars),cex = .7,main = "gas milage for car models",xlab = "Miles per gallon")
+
+# dot chart with colored output 
+ x <- mtcars[order(mtcars$mpg),]
+  x$cyl <- factor(x$cyl)
+  x$color[x$cyl == 4] <-"red"
+   x$color[x$cyl == 6] <-"blue"
+   x$color[x$cyl == 8] <-"green"
+   dotchart(x$mpg,labels = row.names(x),cex = .7,groups = x$cyl,main = "gas milage by cyl",xlab = "miles per gallon",gcolor = "black",color = x$color)
